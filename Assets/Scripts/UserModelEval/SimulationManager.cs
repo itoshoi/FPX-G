50b44befcfd69628a5a41b6ed71f22211e1aa88a
shoi <shoi.itou@gmail.com>
2021-01-06 13:52:58 +0900

    update to multithread

--- Assets/Scripts/UserModelEval/SimulationManager.cs
+++ Assets/Scripts/UserModelEval/SimulationManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
+using System.Threading;
 
 public class SimulationManager : MonoBehaviour
 {
@@ -15,23 +17,73 @@ public class SimulationManager : MonoBehaviour
 
     [SerializeField] private int maxEpoc = 1000;
 
-    private IEnumerator Start()
+	private void Start()
     {
+		StartSimulation();
+		// StartCoroutine(StartSimulationCoroutine());
+		// StartCoroutine(SimulationCoroutine(0.1f, 0));
+    }
+
+	private IEnumerator StartSimulationCoroutine(){
+        Debug.Log("Start Simulation");
         for (float l = startLambda; l < endLambda; l += 0.1f)
         {
-            yield return StartCoroutine(SimulationCoroutine(l, 0));
+			yield return StartCoroutine(SimulationCoroutine(l, 0));
         }
-		
+
 		// wiki data
 		yield return StartCoroutine(SimulationCoroutine(10, 0.1f));
 
         Debug.Log("All Done.");
-
 		yield break;
-    }
+	}
+
+	private async void StartSimulation(){
+        Debug.Log("Start Simulation");
+
+		var tasks = new List<Task>();
+        for (float l = startLambda; l < endLambda; l += 0.1f)
+        {
+			var task_l = l;
+			var task = Task.Run(() => {Simulate(task_l, 0);});
+			// await task;
+			tasks.Add(task);
+        }
+
+		// wiki data
+		// tasks.Add(Task.Run(() => {Simulate(10, 0.1f);}));
+
+		await Task.WhenAll(tasks);
+        Debug.Log("All Done.");
+	}
+
+	private void Simulate(float lambda, float pReturnFirst){
+        Debug.Log(lambda + " Start.");
+
+		var userModel = new VRInterfaceModel();
+        userModel.Lambda = lambda;
+        userModel.dataSet = dataSet;
+		userModel.allNodeCount = allNodeCount;
+		userModel.meanDegree = meanDegree;
+		userModel.distanceFtoG = distanceFtoG;
+		userModel.goalPriority = goalPriority;
+		// 始点ノードに戻る確率
+		userModel.pReturnFirst = pReturnFirst;
+        for (int i = 0; i < maxEpoc; i++)
+        {
+			progress = (float)i / (float)maxEpoc;
+            userModel.Simulate();
+			Thread.Sleep(16);
+        }
+
+        SimDataWriter.WriteData(userModel.Records, dataSet, "lambda_" + lambda.ToString("F1").Replace(".", "_") + ".csv");
+        Debug.Log(lambda + " Done.");
+	}
 
     private IEnumerator SimulationCoroutine(float lambda, float pReturnFirst)
     {
+        Debug.Log(lambda + " Start.");
+
 		var userModel = new VRInterfaceModel();
         userModel.Lambda = lambda;
         userModel.dataSet = dataSet;
